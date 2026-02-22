d5133627d1bbd38aa2e1d2fda77285cf09baf2f7
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-02-22 17:07:58 +0000

    tutorial set up

--- src/tutorials.ts
+++ src/tutorials.ts
@@ -1,1 +1,13 @@
 console.log('rufai with typescript');
+
+interface student {
+    name: string;
+    ID: number;
+}
+
+let student1: student = {
+    name: 'rufai',
+    ID: 1234,
+};
+console.log(student1);
+
