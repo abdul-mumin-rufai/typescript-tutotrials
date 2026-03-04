ab8751e4d15f71521a2cff9299f37e4b11a5b940
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-03-04 22:04:05 +0000

    type annonation and inference: learnt type annotation and type inference.

--- src/tutorials.ts
+++ src/tutorials.ts
@@ -25,5 +25,27 @@ let student: studentInfo = {
     father: 'Rufai Bawa',
     elementarySchool:'Danfa Methodist JHS'
 }
-
 console.log(student);
+
+// type annotation and type inference for different data types
+// typeScript is smart in enough to infer the data types for you, it is normally acceptable annotate the type.
+
+let myVarialbe:string = 'to do is to dare';
+myVarialbe
+
+let myAge: number = 90;
+myAge
+
+let isComingHome: boolean = false;
+isComingHome
+
+// code challenge
+let name: string = 'rufai abdul-mumin';
+name
+
+let myNumber: number = 1000;
+myNumber
+
+
+let myBoolean: boolean = 2 > 9;
+myBoolean
