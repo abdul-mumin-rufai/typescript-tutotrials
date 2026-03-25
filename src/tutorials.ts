075329b3cb74e49274593fa6c456d54b0964b3e5
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-03-25 22:37:05 +0000

    type guard: completed Discriminated Unions

--- src/tutorials.ts
+++ src/tutorials.ts
@@ -884,3 +884,43 @@ if (isJohanPhilosiophy(fifa)) {
 } else {
     fifa.manToMan();
 }
+
+// type guard (Discriminated Unions and exhaustive check using the never type)
+
+type IncrementAction = {
+    type: 'increment';
+    amount: number;
+    timestamp: number;
+    user: string;
+};
+
+type DecrementAction = {
+    type: 'decrement';
+    amount: number;
+    timestamp: number;
+    user: string;
+};
+
+type Action = IncrementAction | DecrementAction;
+
+function reducer(state: number, action: Action):number {
+     switch (action.type) {
+        case 'increment':
+            return state + action.amount
+        case 'decrement':
+             return state - action.amount
+         default:
+             const unexpectedAction: never = action;
+             throw new Error(`hey engineer there was an error: ${unexpectedAction}`)
+    }
+}
+
+const newAction: Action = {
+    type: 'increment',
+    amount: 90,
+    timestamp: 1229,
+    user: 'Abdul-Mumin'
+};
+
+console.log(newAction);
+reducer(90, newAction);
