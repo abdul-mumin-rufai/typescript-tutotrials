0421b7c825945ce62027bfdbea3953424730f011
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-04-05 18:28:20 +0000

    tasksProject: started a min project in order to use  the typescript knowledge i hava learnt so far

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ import './es6modules.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
+    <a href="/task" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
+    <a href="/task" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
